8adcfcb7171f80e12f0e89a9d65807d887f2ee07
“Ventura Lastrucci” <ventura.cc.21.22@gmail.com>
2021-11-20 20:17:29 +0100

    Actualizar version de Project.toml. Eliminar linea de ejecución inecesaria en make.jl

--- make.jl
+++ make.jl
@@ -4,8 +4,5 @@ cd(@__DIR__)
 using Pkg
 # Inicializamos el entorno de este repositorio
 Pkg.activate(".")
-# Instalamos las dependencias especificadas en el Project.toml
-# generando el archivo Manifest.toml
-Pkg.instantiate()
 # Inicializamos los tests
 Pkg.test()
