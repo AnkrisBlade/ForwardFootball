c5c71f68dd37d21a3129d9e7c806ac0a26b1eb2d
Ventura Lastrucci <ventura.cc.21.22@gmail.com>
2022-09-04 20:42:14 +0200

    close #28; Optimización y actualización de distintos archivos

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,30 @@
 # Hacemos uso del contenedor base oficial de julia, con version 1.6
 FROM julia:1.6
 
-# Copiamos los archivos del repositorio al contenedor
-COPY ./ .
+# Definimos el usuario sin privilegios
+ENV USERNAME=ForwardFootball
+ENV USER_UID=1000
+
+RUN groupadd --gid ${USER_UID} ${USERNAME} \
+    && useradd --uid ${USER_UID} --gid ${USER_UID} -m ${USERNAME} \
+    && chown -R ${USER_UID}:${USER_UID} /home/${USERNAME}
+
+# Definimos el directorio de trabajo
+WORKDIR /ForwardFootball
+
+# Copiamos los archivos necesarios para la ejecución de los tests
+COPY Project.toml .
+COPY make.jl .
+
+COPY ./src/. src
+
+COPY ./test/. test
 
 # Ejecutamos el gestor de tareas
 RUN julia make.jl
 
-# Realizamos los tests
+# Inizializamos el usuario sin privilegios
+USER ${USERNAME}
+
+# Definimos el comando a ejecutar cuando se inicializa el contenedor
 CMD ["julia", "make.jl"]
--- test/lectura_tests.jl
+++ test/lectura_tests.jl
@@ -3,18 +3,20 @@ using Test
 
 ## Conjunto de Tests sobre lectura
 
-@testset "Lectura de Datos Básicos" begin
-    path = "../src/Data/Info_Basica/Orange Lions V20-2 vs Orange Lions V20-1/The data report of Orange Lions V20-120200122.xlsx"
-    @test leer_BasicInfo(path).match_id == "38986AF3-4EC0-4EE0-AD8F-7569BB343ADC"
-    @test size(leer_BasicInfo(path).basic_info) == (8, 2)
-    @test leer_BasicInfo(path).team_id == "4967"
-    @test size(leer_BasicInfo(path).team_info) == (5, 53)
+@testset "Lectura de Datos sobre un Partido" begin
+    path = "../src/data/Info_Basica/Orange Lions V20-2 vs Orange Lions V20-1/The data report of Orange Lions V20-120200122.xlsx"
+    @info "Testeo de la lectura de los datos sobre un partido"
+    @test lectura_BasicInfo(path).match_id == "38986AF3-4EC0-4EE0-AD8F-7569BB343ADC"
+    @test size(lectura_BasicInfo(path).basic_info) == (8, 2)
+    @test lectura_BasicInfo(path).team_id == "4967"
+    @test size(lectura_BasicInfo(path).team_info) == (5, 53)
 end
 
-@testset "Lectura de Datos de Entrenamiento" begin
-    path = "../src/Data/Info_Entrenamiento/Orange Lions V20-1/58D7EBEB71C74AAB897D20493B15D912.xlsx"
-    @test leer_TrainInfo(path).train_id == "58D7EBEB71C74AAB897D20493B15D912"
-    @test leer_TrainInfo(path).team_id == "4967"
-    @test length(leer_TrainInfo(path).train_physical) == 4
-    @test length(leer_TrainInfo(path).train_bpm) == 4
+@testset "Lectura de Datos sobre un Entrenamiento" begin
+    path = "../src/data/Info_Entrenamiento/Orange Lions V20-1/58D7EBEB71C74AAB897D20493B15D912.xlsx"
+    @info "Testeo de la lectura de los datos sobre un entrenamiento"
+    @test lectura_TrainInfo(path).train_id == "58D7EBEB71C74AAB897D20493B15D912"
+    @test lectura_TrainInfo(path).team_id == "4967"
+    @test length(lectura_TrainInfo(path).train_physical) == 4
+    @test length(lectura_TrainInfo(path).train_bpm) == 4
 end
